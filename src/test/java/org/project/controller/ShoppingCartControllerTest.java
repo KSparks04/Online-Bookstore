057fc31aa0adadb17660fb754b9d583abf9a53c2
John Guo <john471g@gmail.com>
2025-11-09 00:28:44 -0500

    Added tests

---
+++ src/test/java/org/project/controller/ShoppingCartControllerTest.java
@@ -0,0 +1,61 @@
+package org.project.controller;
+
+import org.junit.jupiter.api.Test;
+import org.project.model.Book;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockHttpSession;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class ShoppingCartControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    /**
+     * Tests the get shopping cart method
+     * @throws Exception
+     */
+    @Test
+    void getShoppingCart() throws Exception {
+        MockHttpSession session = null;
+        Book book = new Book(201, "Title 201", "Author 201", "Publisher 201", "Description 201", 15, 33.95, 4);
+        this.mockMvc.perform(multipart("/add-book").param("ISBN","201").param("title","Title 201").param("author","Author 201")
+                .param("publisher","Publisher 201").param("description","Description 201").param("inventory","15").param("price","33.95").param("pageCount", "4"));
+        this.mockMvc.perform(get("/shopping-cart")).andDo(print()).andExpect(status().isOk())
+                .andExpect(content().string(containsString("No Books in Shopping Cart")));
+    }
+
+    /**
+     * Tests the edit shopping cart method
+     * @throws Exception
+     */
+    @Test
+    void editShoppingCart() throws Exception {
+        MockHttpSession session = null;
+        Book book = new Book(123, "Sample Title", "Author Name", "Publisher Co", "Sample description", 1, 1.0, 5);
+        MockMultipartFile file = new MockMultipartFile("pictureUpload", "file.jpeg", "image/jpeg", "Hello World".getBytes());
+        this.mockMvc.perform(multipart("/add-book").file(file).param("ISBN","123").param("title","Sample Title").param("author","Author Name")
+                .param("publisher","Publisher Co").param("description","Sample description").param("inventory","1").param("price","1.0").param("pageCount", "5")).andExpect(status().is2xxSuccessful());
+
+        //Test add
+        session = (MockHttpSession) this.mockMvc.perform(post("/shopping-cart/edit/add/123")).andDo(print()).andReturn().getRequest().getSession();
+        session = (MockHttpSession) this.mockMvc.perform(get("/shopping-cart").session(session)).andDo(print()).andExpect(status().isOk())
+                .andExpect(content().string(containsString("Sample Title"))).andReturn().getRequest().getSession();
+//      //Test remove
+        session = (MockHttpSession) this.mockMvc.perform(post("/shopping-cart/edit/remove/123")).andDo(print()).andReturn().getRequest().getSession();
+        this.mockMvc.perform(get("/shopping-cart").session(session)).andDo(print()).andExpect(status().isOk())
+                .andExpect(content().string(containsString("No Books in Shopping Cart")));
+    }
+}
