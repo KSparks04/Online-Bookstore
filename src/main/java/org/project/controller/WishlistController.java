69b64cebae930c6cb31ffcd0a03c2b9913bdfbd2
JohnG471 <john471g@gmail.com>
2025-11-26 15:00:14 -0500

    Added html and controller

---
+++ src/main/java/org/project/controller/WishlistController.java
@@ -0,0 +1,78 @@
+package org.project.controller;
+
+import jakarta.servlet.http.HttpSession;
+import org.project.model.Book;
+import org.project.model.ShoppingCart;
+import org.project.model.User;
+import org.project.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class WishlistController {
+    @Autowired
+    private BookRepository bookRepository;
+
+    @GetMapping("/wishlist")
+    public String getWishlist(Model model, HttpSession session) {
+        ShoppingCartController.addShoppingCartAttributes(model, session);
+        User user = (User) session.getAttribute("currentUser");
+        model.addAttribute("wishlist", user.getWishlist());
+
+        // If user is not logged in → redirect to register
+        if (user == null) {
+            session.setAttribute("redirectAfterRegister", "/wishlist");
+            return "redirect:/register";
+        }
+
+        return "wishlist";
+    }
+
+    @GetMapping("/wishlist/fragment")
+    public String getWishlistBody(Model model, HttpSession session) {
+        ShoppingCartController.addShoppingCartAttributes(model, session);
+        User user = (User) session.getAttribute("currentUser");
+        model.addAttribute("wishlist", user.getWishlist());
+        return "fragments/wishlist-body";
+    }
+
+    @PostMapping("/wishlist/edit/{function}/{ISBN}")
+    public String editWishlist(@PathVariable("function") String function,
+                                   @PathVariable("ISBN") long ISBN,
+                                   Model model, HttpSession session) {
+
+        User user = (User) session.getAttribute("currentUser");
+        Book book = bookRepository.findByISBN(ISBN);
+
+        // If user is not logged in → redirect to register
+        if (user == null) {
+            session.setAttribute("redirectAfterRegister", "/get-browse-view");
+            return "redirect:/register";
+        }
+
+        if (function.equalsIgnoreCase("add")) {
+            user.addBookToWishlist(book);
+        } else if (function.equalsIgnoreCase("remove")) {
+            user.removeBookFromWishlist(book);
+        }
+
+        return "redirect:/wishlist";
+    }
+
+    @PostMapping("/wishlist/validate")
+    public String checkOut(Model model, HttpSession session) {
+        User user = (User) session.getAttribute("currentUser");
+
+        // If user is not logged in → redirect to register
+        if (user == null) {
+            session.setAttribute("redirectAfterRegister", "/wishlist");
+            return "redirect:/register";
+        }
+
+        return "redirect:/wishlist";
+    }
+}
--- src/main/java/org/project/model/User.java
+++ src/main/java/org/project/model/User.java
@@ -2,6 +2,9 @@ package org.project.model;
 
 import jakarta.persistence.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Table(name = "users", uniqueConstraints = @UniqueConstraint(columnNames = "username"))
 public class User {
@@ -16,11 +19,15 @@ public class User {
     @Column(nullable = false)
     private String password;
 
+    @OneToMany(mappedBy = "ISBN")
+    private List<Book> wishlist;
+
     public User() {}
 
     public User(String username, String password) {
         this.username = username;
         this.password = password;
+        this.wishlist = new ArrayList<>();
     }
 
     public int getId() {
@@ -41,5 +48,25 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public List<Book> getWishlist() {
+        return this.wishlist;
+    }
+
+    public void setWishlist(List<Book> wishlist) {
+        this.wishlist = wishlist;
+    }
+
+    public boolean addBookToWishlist(Book book){
+        if(this.wishlist.contains(book)){
+            return false;
+        }
+        this.wishlist.add(book);
+        return true;
+    }
+
+    public boolean removeBookFromWishlist(Book book){
+        return this.wishlist.remove(book);
     }
 }
